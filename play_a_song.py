5a58b62ae6bc49dae5842c8aa41b76e75a4234e7
Alvaro <alvarote9999@hotmail.com>
2017-12-04 15:10:40 +0100

    Update play_a_song.py

--- play_a_song.py
+++ play_a_song.py
@@ -7,6 +7,6 @@ Config=ConfigParser.ConfigParser()
 try:
   Config.read('alarm.config')
 except:
-  raise Exception('Sorry, Failed reading alarm.config file.')
+  raise Exception('Disculpe, Error al leer el archivo alarm.config .')
 
 subprocess.call('find '+ Config.get('main','musicfldr') + ' -name \'*.mp3\' | sort --random-sort| mpg123 -@ - -l 1 -g 60', shell=True)
